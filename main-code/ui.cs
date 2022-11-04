7c9e1c2a57082eae367f110a7acb69b2e85f4ae7
Top-Cat <67204693+blobhead@users.noreply.github.com>
2022-11-04 08:06:15 +0000

    Update ui.cs

--- main-code/ui.cs
+++ main-code/ui.cs
@@ -1,2 +1,18 @@
-use system;
+using system;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void MethodOne()
+    {
+    Console.WriteLine("Test");
+    }
+    static void Main(string[] args)
+    {
+      MethodONe();
+      Console.WriteLine("Test Two");    
+    }
+  }
+}
 
