e68b945079a71964f7b7caec676e5cbd774c568d
Top-Cat <67204693+blobhead@users.noreply.github.com>
2022-11-04 08:03:04 +0000

    Update internet-searcher.rs

--- main-code/internet-searcher.rs
+++ main-code/internet-searcher.rs
@@ -1,2 +1,20 @@
+use std::prelude;
+
+let mut web_tag: String::from("Vitrax") 
+let results_number: i32: 0;
+
 fn main() {
+  
+};
+
+fn search_web {
+
+};
+
+fn print_results {
+
+};
+
+fn encrypt {
+
 };
